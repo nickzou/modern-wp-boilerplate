f27a66851272119022f9bf0b73a69eedb2fa083f
Nick Zou <z.nick00@gmail.com>
2025-03-09 22:34:33 -0400

    add styles

--- web/wp-content/themes/base-theme/inc/enqueue_theme_assets.php
+++ web/wp-content/themes/base-theme/inc/enqueue_theme_assets.php
@@ -10,6 +10,8 @@
 function enqueue_theme_assets()
 {
     wp_enqueue_script('app');
+    wp_enqueue_style('tailwind');
+    wp_enqueue_style('styles');
 };
 
 add_action('wp_enqueue_scripts', 'enqueue_theme_assets');
