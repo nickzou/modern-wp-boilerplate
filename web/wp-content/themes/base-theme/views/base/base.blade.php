c0faf016ff8a42e1a95d53d2cc4871bc67cd0c4d
Nick Zou <z.nick00@gmail.com>
2025-04-04 21:02:14 -0400

    use directives in base

--- web/wp-content/themes/base-theme/views/base/base.blade.php
+++ web/wp-content/themes/base-theme/views/base/base.blade.php
@@ -3,21 +3,15 @@
 <head>
     <meta charset="{{ $charset }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {!! $wp_head !!}
+    @wphead
 </head>
 
 <body class="{{ $body_class }}">
-        {!! $wp_body_open !!}
+    @wpbodyopen
 
-    <header>
-        <h1><a href="{{ $home_url }}">{{ $site_name }}</a></h1>
-        <p>{{ $description }}</p>
-    </header>
-
-    <footer>
-        <p>&copy; {{ $copyright_year }} {{ $site_name }}</p>
-    </footer>
-
-    {!! $wp_footer !!}
+    @include('globals.header')
+        yotyot
+    @include('globals.footer')
+    @wpfooter
 </body>
 </html>
