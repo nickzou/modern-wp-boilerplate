a596cd6b1d1fb8eee6d4215e096fb5e9e0fbcc24
Nick Zou <z.nick00@gmail.com>
2025-07-12 14:29:35 -0400

    add alpine

--- web/wp-content/themes/base-theme/inc/register_theme_js.php
+++ web/wp-content/themes/base-theme/inc/register_theme_js.php
@@ -10,6 +10,27 @@ function register_theme_js_files()
     $theme_directory = get_template_directory();
     $theme_uri = get_template_directory_uri();
     wp_register_script("app", "{$theme_uri}/js/app.js", [], filemtime("{$theme_directory}/js/app.js"));
+    wp_register_script(
+        "alpine",
+        "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js",
+        ["alpine-intersect"],
+        "3.x.x",
+        [
+            "strategy" => "defer",
+            "in_footer" => false,
+        ]
+    );
+
+    wp_register_script(
+        "alpine-intersect",
+        "https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js",
+        [],
+        "3.x.x",
+        [
+            "strategy" => "defer",
+            "in_footer" => false,
+        ]
+    );
 }
 
 /**
