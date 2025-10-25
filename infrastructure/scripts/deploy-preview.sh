0ca12db7f7d35783201447bf30552c25186a978c
Nick Zou <z.nick00@gmail.com>
2025-10-25 15:31:00 -0400

    remove todo list

--- infrastructure/scripts/deploy-preview.sh
+++ infrastructure/scripts/deploy-preview.sh
@@ -15,14 +15,6 @@ fi
 
 echo "Deploying preview for branch: $BRANCH_NAME"
 
-# TODO: Sanitize branch name (remove special chars)
-# TODO: Create CloudFlare DNS record
-# TODO: Create database
-# TODO: Install WordPress
-# TODO: Generate nginx config
-# TODO: Get SSL cert
-# TODO: Reload nginx
-
 # Sanitize branch name (replace / and _ with -)
 SAFE_NAME=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9-]/-/g' | tr '[:upper:]' '[:lower:]')
 
